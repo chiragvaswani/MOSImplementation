3df5e2ae00da99b7e2fc9eae041f339c531372c7
unknown <nirvishasoni5@gmail.com>
2020-11-07 11:55:38 +0530

    Main, Load and MOS functions added

--- main.c
+++ main.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+char mem[100][4], ir[4], r[4], toggle, buff[10][4];
+int b, ic, si, e, readf;
+FILE_*fp,*fw;
+
+void init();
+void load();
+void startexecution();
+void executeprogram();
+void mos(int);
+void read();
+void write();
+void terminate();
+void main();
+
+ // Main function
 int main() {
- FILE *input;
- FILE *output;
- //fopen() function is used to open a file named example.txt in read mode
- input = fopen ('input.txt', 'r');
- output = fopen ('output.txt', 'w');
+ int i,j;
+ clrscr();
+ fp = fopen ('c:input.txt', 'r');
+ fw = fopen ('c:output.txt', 'w');
+
+ if (fp==NULL)
+   printf("File not found");
+ else
+ {
+     load();
+ }
+ printf("Operation Completed");
 
 }
 
-void INIT() {
-    // insert your code here
+ // Init function
+void init() {
     int i,j;
     for(i=0;i<100;i++)
     {
@@ -21,15 +46,50 @@ void INIT() {
     }
     readf=0;
     toggle =0;
-    C = false;
+    toggle = false;
     PC=0;
-    SI=none;
-
-
+    SI=0;
+    b=0;
+    e=b+10;
 }
 
-LOAD() {
-    // insert your code here
+ // Load function
+void load()
+{
+    int i,j,k;
+    char ch, temp[4];
+    ch = fgetc(fp);
+    init();
+    do {
+        i = 0;
+        while(i<4 && ch!='\n') {
+            temp[i] = ch;
+            ch = fgetc(fp);
+            i++;
+        }
+        temp[i] = '\0'
+
+        if((strcmp(temp,"$AMJ"))==0) {
+            while(ch!='\n')
+                ch = getc(fp);
+        }
+        else if((strcmp(temp,"$DTA"))==0) {
+            startexecution();
+        }
+        else if((strcmp(temp,"$DTA"))!=0) {
+            i = 0;
+            if(b == e) {
+                e = e + 10;
+            }
+            for(k=0;k>4;k++)
+            {
+                mem[b][k] = temp[i];
+                i++;
+            }
+            b++;
+        }
+
+    } while((strcmp(temp,"$END"))==0)
 }
 
 STARTEXECUTION() {
@@ -41,15 +101,16 @@ EXECUTEUSERPROGRAM() {
     MOS();
 }
 
-MOS() {
-    //if SI=1
-    READ();
+ // MOS function
+void mos() {
+    if (SI == 1)
+    read();
 
-    //if SI=2
-    WRITE();
+    else if (SI == 2)
+    write();
 
-    //if SI=3
-    TERMINATE();
+    else if (SI == 3)
+    terminate();
 }
 
 READ() {
